f0ced2da8e866c32f9292ddfbf5a754d987c9c56
Sergey <chechelsk@gmail.com>
2020-09-15 17:03:37 +0300

    second commit

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 // 'use strict';
 
-let money, time, stage, amount;
+let money, time, a1, a2, a3, a4;
 
-    money = prompt('Ваш бюджет на месяц?');
-    stage = prompt('Введите обязательную статью расходов в этом месяце');
-    amount = prompt('Во сколько обойдется?');
+    money = +prompt('Ваш бюджет на месяц?');
     time = prompt('Введите дату в формате YYYY-MM-DD');
-    
+        
     let appData = {
-        money: money,
+        budget: money,
         timeData: time,
-        expenses: {
-                stage: amount
-            },
-        optionalExpenses: '',
-        income: '',
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
         savings: false 
     }
 
-alert('бюджет на 1 день ' + Math.round(appData['money']/30, 2));
+    for (let i = 0; i < 2; i++) {
+        a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
+        if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
+            && a != '' && b != '' && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            alert('Вы ввели данные в неверном формате, повторите ввод')
+            i -= 1;
+            continue;
+        }
+        
+    }   
 
+appData.moneyPerDay = Math.round(appData.budget/30, 2);
 
+console.log('бюджет на 1 день ', appData.moneyPerDay);
