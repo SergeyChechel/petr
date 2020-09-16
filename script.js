76be39b5a00d8da210d58b43ee11d523be160a7e
Sergey <chechelsk@gmail.com>
2020-09-16 09:48:08 +0300

    lesson 10

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 // 'use strict';
 
-let money, time, a1, a2, a3, a4;
+function detectDayBudget() {
+    let money, time;
 
-    money = +prompt('Ваш бюджет на месяц?');
-    time = prompt('Введите дату в формате YYYY-MM-DD');
-        
-    let appData = {
-        budget: money,
-        timeData: time,
-        expenses: {},
-        optionalExpenses: {},
-        income: [],
-        savings: false 
+        money = +prompt('Ваш бюджет на месяц?');
+        time = prompt('Введите дату в формате YYYY-MM-DD');
+            
+        let appData = {
+            budget: money,
+            timeData: time,
+            expenses: {},
+            optionalExpenses: chooseOptExpenses(),
+            income: [],
+            savings: false 
+        }
+
+    appData.moneyPerDay = (appData.budget/30).toFixed(2);
+    alert('Ваш бюджет на 1 день составляет ' + appData.moneyPerDay + 'гривен');
+}
+
+function detectLevel(summ) {
+
+    if(summ < 100) {
+       return 'Доход низкий'; 
+    }
+    if(summ > 100 && summ < 1000) {
+        return 'Доход средний'; 
+    }
+    if(summ > 1000) {
+        return 'Доход высокий'; 
     }
 
-    for (let i = 0; i < 2; i++) {
-        a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-        b = prompt('Во сколько обойдется?', '');
-        if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
-            && a != '' && b != '' && a.length < 50) {
-            appData.expenses[a] = b;
-        } else {
+}
+
+function chooseOptExpenses() {
+    let optExpenses = {};
+    for (let i = 0; i < 3; i++) {
+        a = prompt('Статья необязательных расходов?', '');
+        if(typeof(a) === 'string' && typeof(a) != null  
+            && a != '' && a.length < 50) {
+                optExpenses[i] = a;
+        } 
+        else {
             alert('Вы ввели данные в неверном формате, повторите ввод')
             i -= 1;
             continue;
         }
-        
-    }   
+    }
+    return optExpenses; 
+}
 
-appData.moneyPerDay = Math.round(appData.budget/30, 2);
-
-console.log('бюджет на 1 день ', appData.moneyPerDay);
+detectDayBudget();
